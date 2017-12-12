420a4910ad24f3bc9a4502de7ca133beb102e78f
Bruno <brunoprogramadorjava@gmail.com>
2017-12-12 21:55:45 -0200

    Changing to compare by size

--- src/com/brunosouza/study/SortStrings.java
+++ src/com/brunosouza/study/SortStrings.java
@@ -2,6 +2,7 @@ package com.brunosouza.study;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortStrings {
@@ -14,10 +15,25 @@ public class SortStrings {
 		names.add("Miguel");
 		names.add("Ana Julia");
 		
-		Collections.sort(names);
+		Comparator<String> comparatorBySize = new ComparatorBySize();
+		Collections.sort(names, comparatorBySize);
 		
 		for (String name : names) {
 			System.out.println(name);
 		}
 	}
 }
+
+
+class ComparatorBySize implements Comparator<String>{
+
+	@Override
+	public int compare(String s1, String s2) {
+		if(s1.length() < s2.length())
+			return -1;
+		if(s1.length() > s2.length())
+			return 1;
+		return 0;
+	}
+}
+
